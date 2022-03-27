fa135dc74da0e92f6b2e0f763af6a2bf0b03b22c
Justine <justine.maugeais@etu.univ-tours.fr>
2022-03-27 06:48:49 -0400

    Début travail visuel page profil

--- lib/pages/user/Profile.dart
+++ lib/pages/user/Profile.dart
@@ -47,8 +47,17 @@ class _ProfileState extends State<Profile> {
       ),
 
       decoration: BoxDecoration(
-          color: Colors.yellow,
-          borderRadius: BorderRadius.circular(10)
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(10), //border corner radius
+        boxShadow:[
+          BoxShadow(
+            color: Colors.black.withOpacity(0.6), //color of shadow
+            blurRadius: 3, // blur radius
+            offset: const Offset(0, 1), // changes position of shadow
+            //first paramerter of offset is left-right
+            //second parameter is top to down
+          ),
+        ],
       ),
       padding: const EdgeInsets.only(left: 20, top:10, right: 20, bottom:10),
       margin: const EdgeInsets.only(left: 20, top:10, right: 20, bottom:10),
@@ -87,7 +96,16 @@ class _ProfileState extends State<Profile> {
       margin: const EdgeInsets.only(left: 20, top:10, right: 20, bottom:10),
       decoration: BoxDecoration(
         color: Colors.yellow,
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(10), //border corner radius
+        boxShadow:[
+          BoxShadow(
+            color: Colors.black.withOpacity(0.6), //color of shadow
+            blurRadius: 3, // blur radius
+            offset: const Offset(0, 1), // changes position of shadow
+            //first paramerter of offset is left-right
+            //second parameter is top to down
+          ),
+        ],
       ),
     );
   }
