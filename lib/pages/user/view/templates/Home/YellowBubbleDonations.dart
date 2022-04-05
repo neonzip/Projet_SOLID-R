c239ea117893bb30dcfd13e4be4469ba59a61c45
Justine <justine.maugeais@etu.univ-tours.fr>
2022-04-05 10:09:14 -0400

    Bulle jaune des dons réalisés renvoie sur la page des projets soutenus.

--- lib/pages/user/view/templates/Home/YellowBubbleDonations.dart
+++ lib/pages/user/view/templates/Home/YellowBubbleDonations.dart
@@ -17,48 +17,52 @@ class _YellowBubbleDonationsState extends State<YellowBubbleDonations> {
 
   /// Yellow bubble containing the donations that the user made.
   Widget yellowBubbleDonations() {
-    return Container(
-      child:
-      Column(
-        children: [
-          const Text(
-              "Dons réalisés :",
-              style: TextStyle(fontSize: 20)
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+    return GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, "/projects/section=formal");
+        },
+        child: Container(
+          child:
+          Column(
             children: [
-              Text(
-                  widget.valueDonations,
-                  style: const TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold)
-              ),
-
               const Text(
-                  " €",
+                  "Dons réalisés :",
                   style: TextStyle(fontSize: 20)
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                      widget.valueDonations,
+                      style: const TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold)
+                  ),
+
+                  const Text(
+                      " €",
+                      style: TextStyle(fontSize: 20)
+                  ),
+                ],
               ),
             ],
           ),
-        ],
-      ),
-      decoration: BoxDecoration(
-        color: Colors.yellow,
-        borderRadius: BorderRadius.circular(10), //border corner radius
-        boxShadow:[
-          BoxShadow(
-            color: Colors.black.withOpacity(0.6), //color of shadow
-            blurRadius: 3, // blur radius
-            offset: const Offset(0, 1), // changes position of shadow
-            //first paramerter of offset is left-right
-            //second parameter is top to down
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(10), //border corner radius
+            boxShadow:[
+              BoxShadow(
+                color: Colors.black.withOpacity(0.6), //color of shadow
+                blurRadius: 3, // blur radius
+                offset: const Offset(0, 1), // changes position of shadow
+                //first paramerter of offset is left-right
+                //second parameter is top to down
+              ),
+            ],
           ),
-        ],
-      ),
-      padding: const EdgeInsets.only(left: 20, top:10, right: 20, bottom:10),
-      margin: const EdgeInsets.only(left: 20, top:10, right: 20, bottom:10),
+          padding: const EdgeInsets.only(left: 20, top:10, right: 20, bottom:10),
+          margin: const EdgeInsets.only(left: 20, top:10, right: 20, bottom:10),
+        )
     );
   }
-
 }
