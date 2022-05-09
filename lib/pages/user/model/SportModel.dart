b3a2127f0816ce8bd8f619a7c0e24a08bb0ca8b1
Imane Moksit <imane.moksit@etu.univ-tours.fr>
2022-05-09 02:02:33 +0200

    sportDAO add/get/delete

--- lib/pages/user/model/SportModel.dart
+++ lib/pages/user/model/SportModel.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 class SportModel {
   late int sportID;
   late String sportName;
@@ -10,4 +12,17 @@ class SportModel {
     sportConversionRate = rate;
     sportMaxSpeed = speed;
   }
+
+  SportModel.fromJson(Map<dynamic, dynamic> json)
+      : sportName = json['name'] as String,
+        sportConversionRate = double.parse(json['convertionRate'] as String),
+        sportMaxSpeed = double.parse(json['maxSpeed'] as String);
+
+
+  Map<dynamic, dynamic> toJson() => <dynamic, dynamic>{
+    'sport_id' : sportID,
+    'name': sportName,
+    'convertionRate': sportConversionRate.toString(),
+    'maxSpeed':  sportMaxSpeed.toString(),
+  };
 }
