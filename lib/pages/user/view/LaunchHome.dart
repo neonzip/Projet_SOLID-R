41497ba6dc3964d6bf679fe8e5558c57d832916d
Justine <justine.maugeais@etu.univ-tours.fr>
2022-05-11 06:29:31 -0400

    Chgt fond d'écran appli connexion.

--- lib/pages/user/view/LaunchHome.dart
+++ lib/pages/user/view/LaunchHome.dart
@@ -52,7 +52,14 @@ class _LaunchHomeState extends State<LaunchHome> {
                       gradient: RadialGradient(
                         radius: 0.6,
                         colors: [
+                          const Color(0xFF0725A5).withOpacity(0.2),
+                          const Color(0xFF0725A5).withOpacity(0.4),
+                          const Color(0xFF0725A5).withOpacity(0.6),
+                          const Color(0xFF0725A5).withOpacity(0.7),
                           const Color(0xFF0725A5).withOpacity(0.8),
+                          const Color(0xFF0725A5).withOpacity(0.9),
+                          const Color(0xFF0725A5),
+                          const Color(0xFF0725A5),
                           const Color(0xFF0725A5),
                         ],
                       )
