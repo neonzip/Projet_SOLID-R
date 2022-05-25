ae553fd0da08a8b117a611d3e83451b7e632360e
Imane Moksit <imane.moksit@etu.univ-tours.fr>
2022-05-26 00:54:51 +0200

    SportModel

--- lib/pages/user/model/SportModel.dart
+++ lib/pages/user/model/SportModel.dart
@@ -1,5 +1,6 @@
 
 class SportModel {
+
   late int sportID;
   late String sportName;
   late double sportConversionRate;
@@ -13,15 +14,43 @@ class SportModel {
   }
 
   SportModel.fromJson(Map<dynamic, dynamic> json)
-      : sportName = json['name'] as String,
-        sportConversionRate = double.parse(json['convertionRate'] as String),
-        sportMaxSpeed = double.parse(json['maxSpeed'] as String);
+      : sportID = int.parse(json['sportID'] as String),
+        sportName = json['sportName'] as String,
+        sportConversionRate = double.parse(json['sportConversionRate'] as String),
+        sportMaxSpeed = double.parse(json['sportMaxSpeed'] as String);
 
 
   Map<dynamic, dynamic> toJson() => <dynamic, dynamic>{
-    'sport_id' : sportID,
-    'name': sportName,
-    'convertionRate': sportConversionRate.toString(),
-    'maxSpeed':  sportMaxSpeed.toString(),
+    'sportID' : sportID,
+    'sportName': sportName,
+    'sportConversionRate': sportConversionRate.toString(),
+    'sportMaxSpeed':  sportMaxSpeed.toString(),
   };
+
+  /***** GETTERS and SETTERS ******/
+
+  int getSportID(){
+    return sportID;
+  }
+  void setSportID(int id){
+    sportID=id;
+  }
+  String getSportName(){
+    return sportName;
+  }
+  void setSportName(String name){
+    sportName=name;
+  }
+  double getSportConversionRate(){
+    return sportConversionRate;
+  }
+  void setSportConversionRate(double c){
+    sportConversionRate=c;
+  }
+  double getSportMaxSpeed(){
+    return sportMaxSpeed;
+  }
+  void setSportMaxSpeed(double c){
+     sportMaxSpeed=c;
+  }
 }
